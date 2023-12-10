5d9567935889a4ddd76685e527f65a12ae24ac4c
Mahesh <thakkilm@mail.gvsu.edu>
2023-12-10 11:33:08 -0500

    implement code to call hellowrld using thymeleaf template

---
+++ thymeleafdemo/src/main/java/com/mahesh/spring/thymeleafdemo/controller/ThymeleafDemoController.java
@@ -0,0 +1,14 @@
+package com.mahesh.spring.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThymeleafDemoController {
+    @GetMapping("/")
+    public String sayHello(Model theModel){
+        theModel.addAttribute("theDate",new java.util.Date());
+        return "helloworld";
+    }
+}
