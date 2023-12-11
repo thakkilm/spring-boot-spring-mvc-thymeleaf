b74799945b21738c02a95e80fb6e36a732fd0eb0
Mahesh <thakkilm@mail.gvsu.edu>
2023-12-11 01:21:22 -0500

    Added .txt to Revise

--- 01-validationDemo/src/main/java/com/mahesh/spring/validationDemo/validation/CourseCode.java
+++ 01-validationDemo/src/main/java/com/mahesh/spring/validationDemo/validation/CourseCode.java
@@ -8,9 +8,9 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Constraint(validatedBy = CourseCodeConstraintValidator.class)
-@Target({ElementType.METHOD,ElementType.FIELD})
-@Retention(RetentionPolicy.RUNTIME )
+    @Constraint(validatedBy = CourseCodeConstraintValidator.class)
+    @Target({ElementType.METHOD,ElementType.FIELD})
+    @Retention(RetentionPolicy.RUNTIME )
 public @interface CourseCode {
 
     public String value() default "LUV";
