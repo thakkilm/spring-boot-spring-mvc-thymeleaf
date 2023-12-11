c039a84054736c13fc8690756bc0db2333c6b2c4
Mahesh <thakkilm@mail.gvsu.edu>
2023-12-11 14:33:57 -0500

    Implement method to add new employee to the database

--- 03-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
+++ 03-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import com.luv2code.springboot.cruddemo.service.EmployeeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -23,7 +25,17 @@ public class EmployeeController {
     public String istEmployees(Model theModel){
         List<Employee> employees=employeeService.findAll();
         theModel.addAttribute("employees",employees);
-        return "list-employees";
+        return "employees/list-employees";
+    }
 
+    @GetMapping("/showFormForAdd")
+    public String showFormForAdd(Model theModel){
+        theModel.addAttribute("employee",new Employee());
+        return "employees/employee-form";
+    }
+    @PostMapping("/save")
+    public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
+        employeeService.save(theEmployee);
+        return "redirect:/employees/list";
     }
 }
