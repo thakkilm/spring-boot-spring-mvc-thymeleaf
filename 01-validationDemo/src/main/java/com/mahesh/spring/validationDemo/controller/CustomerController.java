c4e4b1c6734ebce98fc7257e520b631e4172b911
Mahesh <thakkilm@mail.gvsu.edu>
2023-12-11 00:19:16 -0500

    use @InitBinder to perform trim operations on String data

--- 01-validationDemo/src/main/java/com/mahesh/spring/validationDemo/controller/CustomerController.java
+++ 01-validationDemo/src/main/java/com/mahesh/spring/validationDemo/controller/CustomerController.java
@@ -2,16 +2,24 @@ package com.mahesh.spring.validationDemo.controller;
 
 import com.mahesh.spring.validationDemo.model.Customer;
 import jakarta.validation.Valid;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CustomerController {
 
+    @InitBinder
+    public void initBinder(WebDataBinder webDataBinder){
+        StringTrimmerEditor stringTrimmerEditor=new StringTrimmerEditor(true);
+        webDataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
     @GetMapping("/")
     public String customerForm(Model theModel){
 
@@ -21,7 +29,7 @@ public class CustomerController {
 
     @PostMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer, BindingResult bindingResult){
-        System.out.println("Called the method");
+
     if(bindingResult.hasErrors()){
         return "customer-form";
     }
