6d69596328942219bb425db4f578a1bcabdb2aa7
Mahesh <thakkilm@mail.gvsu.edu>
2023-12-11 00:39:44 -0500

    use @Patter to perform regex validations

--- 01-validationDemo/src/main/java/com/mahesh/spring/validationDemo/model/Customer.java
+++ 01-validationDemo/src/main/java/com/mahesh/spring/validationDemo/model/Customer.java
@@ -1,9 +1,6 @@
 package com.mahesh.spring.validationDemo.model;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 
 public class Customer {
 
@@ -20,9 +17,22 @@ public class Customer {
     @NotNull(message = "is required")
     @Size(min=1,message="is required")
     private String lastName;
+
+
 @Min(value=0,message = "must be >=0")
 @Max(value=10,message = "must be<=10")
 private int freePasses;
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "Yedava")
+    private String postalCode;
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+
 
     public int getFreePasses() {
         return freePasses;
