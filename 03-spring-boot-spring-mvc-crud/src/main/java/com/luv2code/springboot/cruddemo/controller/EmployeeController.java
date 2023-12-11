a54cc959738b77b37aa23e64d77ec243426b02a6
Mahesh <thakkilm@mail.gvsu.edu>
2023-12-11 11:57:27 -0500

    Initiate new project to work on CRUD with thymeleaf view

---
+++ 03-spring-boot-spring-mvc-crud/src/main/java/com/luv2code/springboot/cruddemo/controller/EmployeeController.java
@@ -0,0 +1,29 @@
+package com.luv2code.springboot.cruddemo.controller;
+
+import com.luv2code.springboot.cruddemo.entity.Employee;
+import com.luv2code.springboot.cruddemo.service.EmployeeService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/list")
+    public String istEmployees(Model theModel){
+        List<Employee> employees=employeeService.findAll();
+        theModel.addAttribute("employees",employees);
+        return "list-employees";
+
+    }
+}
